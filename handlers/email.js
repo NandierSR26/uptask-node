5041aaf67844d580b3975216fa72c1f0e6042fa0
Nandier Ruiz <nandiersruiz@outlook.com>
2022-06-30 21:22:51 -0500

    settings

--- handlers/email.js
+++ handlers/email.js
@@ -6,13 +6,18 @@ const util = require('util');
 const emailConfig = require('../config/email');
 
 let transport = nodemailer.createTransport({
-    host: emailConfig.host,
-    port: emailConfig.port,
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
-      user: emailConfig.user, 
-      pass: emailConfig.pass
-    }
-});
+      type: "OAuth2",
+      user: "nandierruizacosta@gmail.com",
+      clientId: "724751063745-r0hs4h93c6aoiseieds8o20bpt383b69.apps.googleusercontent.com",
+      clientSecret: "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",
+      refreshToken: "1//049QeJedYjJFaCgYIARAAGAQSNwF-L9Ir77JlSNCQSBBK4HCmZ3ZwTpBwtgQeoEWwLWSbhPeSsjWM09Htzy7sybwOmuJhMgn7BjE",
+      accessToken: "ya29.A0ARrdaM-3ZnzmhogA4xgT7Vm1SfL5ApQTIksz66YF9p0z0cWTmLMymE2i4GyLgtmyCUrZ5iT0rpnEvBwzncFQVBImDu_t3yutwOm55IugHXVwMB9lNwAcOHGiqj27qTAgxRaBsHlQkFmiTgun7MCjr6SYc_e-YUNnWUtBVEFTQVRBU0ZRRl91NjFWSjZVSk5CaVk1cUxjRFpOcHdpQ2VEZw0163",
+    },
+  });
 
 // generar HTML
 const generarHTML = (archivo, opciones = {}) => {
